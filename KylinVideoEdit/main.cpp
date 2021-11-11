931196976a3fb85bf74609d0a075efc72fe57d8b
zhuyoucheng <2025221556@qq.com>
2021-11-11 21:24:30 +0800

    终版

--- KylinVideoEdit/main.cpp
+++ KylinVideoEdit/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * author: 朱有成 冉杨 张新蕾
+ * email: 2025221556@qq.com 2578874882@qq.com 2548742654@qq.com
+ * time: 2021.10
+
+ * Copyright (C) <2021> < Youcheng Zhu, Yang Ran, Xinlei zhang >
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License  as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURP0SE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see < https://www.gnu.org/licenses/>.
+ */
 #include <QGuiApplication>
 #include <QApplication>
 #include <QQmlApplicationEngine>
